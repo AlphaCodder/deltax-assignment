6f54605fe46d17d79da6b91aab221a227261358a
Sr Joe <srjoethef@gmail.com>
2022-07-27 21:19:30 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* design a music top 10 site */}
+        <h1>HOME</h1>
+        <h2>Top 10 Songs</h2>
+        <button className='App-btn'>Add Songs</button>
+        <h2>Top 10 Artists</h2>
+        
       </header>
     </div>
   );
